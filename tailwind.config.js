2e71f85aaeaf41d523712f1bf4e9dc9c44a1e97a
Dani <hi@daniguardiola.me>
2020-11-10 22:43:19 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,13 @@ module.exports = {
   purge: ['./src/index.html'],
   future: {
     purgeLayersByDefault: true
+  },
+  theme: {
+    extend: {
+      colors: {
+        brown: '#27140a',
+        green: '#a2c037'
+      }
+    }
   }
 }
