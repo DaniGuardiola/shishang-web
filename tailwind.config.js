4a69a078b97aa5595015627bbe77c9f5d3571e9a
Cesar Rojas <cesar.fieoner@gmail.com>
2020-11-11 19:15:35 +0100

    still ugly but not as much as before

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,11 @@ module.exports = {
   purge: ['./src/index.html'],
   future: {
     purgeLayersByDefault: true
+  },
+  theme: {
+    colors: {
+      'menu-header': '#a2c037',
+      'menu-header-hover': '#a7c53b',
+    }
   }
 }
