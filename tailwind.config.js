225c48261598d2ac802db91db4e2963492afc90c
BuildTools <unconfigured@null.spigotmc.org>
2020-11-14 18:22:31 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,14 @@ module.exports = {
   purge: ['./src/index.html'],
   future: {
     purgeLayersByDefault: true
+  },
+
+  theme: {
+    extend: {
+      colors: {
+        'light-brown': '#AF8F7E',
+        'light-yellow': '#E8FF8B'
+      }
+    }
   }
 }
